c87cc4b95b08af2fce86fb567f8a8dad69c20598
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-31 13:11:43 -0500

    copied over the files

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 //this is our controller!
 
+//start session
+session_start();
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -16,6 +18,51 @@ $f3 = Base::instance();
 $f3->route('GET /', function () {
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+//route for form one
+$f3->route('GET /order', function () {
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+//route for form 2
+$f3->route('POST /order2', function () {
+    $_SESSION['animal'] = $_POST['animal'];
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+//route for results page
+$f3->route('POST /results', function () {
+    $_SESSION['color'] = $_POST['color'];
+    $view = new Template();
+    echo $view->render('views/results.html');
+});
+
+
+$f3->route('GET /@item', function ($f3, $param) {
+    $item = $param["item"];
+    switch($item){
+        case "chicken":
+            echo "<p>Cluck</p>";
+            break;
+        case "dog":
+            echo "<p>Woof</p>";
+            break;
+        case "pig":
+            echo "<p>Oink</p>";
+            break;
+        case "horse":
+            echo "<p>Neigh</p>";
+            break;
+        case "bird":
+            echo "<p>Chirp</p>";
+            break;
+        default:
+            $f3->error(404);
+            break;
+    }
 });
 
 //run fat free
